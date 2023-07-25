4685b8b3ef7392727366bf9c898ec263d31d0661
NaiDang <Naid4243@gmail.com>
2023-07-26 00:40:16 +0700

    new

--- Script.js
+++ Script.js
@@ -1,41 +1,30 @@
+const unreadMessages = document.querySelectorAll(".unread");
+const unread = document.getElementById("notifes");
+const markAll = document.getElementById("mark_all");
+unread.innerText = unreadMessages.length;
 
-
-  // This function will change the status of the message to read
-  function markAsRead(notificationElement) {
-    const unreadIcon = notificationElement.querySelector(".un-read-icon");
-    if (unreadIcon) {
-      unreadIcon.style.display = "none";
-      notificationElement.style.backgroundColor = "#FFF";
-    }
-  }
-
-  // This function will count the number of unread messages
-  function countUnreadNotifications() {
-    const notifications = document.querySelectorAll(".accounts > div");
-    let unreadMessages = 0;
-    notifications.forEach((notification) => {
-      const unreadIcon = notification.querySelector(".un-read-icon");
-      if (unreadIcon) {
-        unreadMessages++;
-      }
-    });
-    return unreadMessages;
-  }
-
-  //Handle event when notifications are clicked
-  document.addEventListener("DOMContentLoaded", function () {
-    const notifications = document.querySelectorAll(".accounts > div");
-    const notificationCount = document.querySelector(".number .count");
-    let unreadMessages = countUnreadNotifications(); // Initial number of unread notifications
-    notificationCount.textContent = unreadMessages;
-
-    // Set click event for each notification
-    notifications.forEach((notification) => {
-      notification.addEventListener("click", function () {
-        markAsRead(notification);
-        unreadMessages--;
-        // Update the number of unread notifications
-        notificationCount.textContent = unreadMessages;
-      });
-    });
+unreadMessages.forEach((message) => {
+  message.addEventListener("click", (event) => {
+    message.classList.remove("unread");
+    updateUnreadCount();
   });
+});
+
+document.addEventListener("click", (event) => {
+  const selectedElement = document.querySelector(".selected");
+  if (selectedElement && !selectedElement.contains(event.target)) {
+    selectedElement.classList.remove("selected");
+  }
+});
+
+markAll.addEventListener("click", () => {
+  unreadMessages.forEach((message) => {
+    message.classList.remove("unread");
+  });
+  updateUnreadCount();
+});
+
+function updateUnreadCount() {
+  const newUnreadMessages = document.querySelectorAll(".unread");
+  unread.innerText = newUnreadMessages.length;
+}
